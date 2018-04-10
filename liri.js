67e8ddbf6c598b50d1bd49f656cb26fc15f012d0
JohnGit54 <johnp701@yahoo.com>
2018-04-09 21:10:29 -0400

    added log.txt

--- liri.js
+++ liri.js
@@ -13,24 +13,15 @@ var Twitter = require('twitter');
 var client = new Twitter(keys.twitter);
 
 
-//var omdb = require('omdb');
 var request = require('request');
 
 var fs = require("fs");
 
 
-function showKeys() {
-    console.log(keys);
-    console.log(keys.spotify);
-    console.log(keys.twitter);
-}
-//showKeys();
-
-
 // Store all of the arguments in an array
 var nodeArgs = process.argv;
 
-var userTitle = ""; //this will hold either the song or movie title
+var userTitle = ""; //this will hold either the requsted song or movie title
 
 // Loop through all the words in the node argument
 // And do a little for-loop magic to handle the inclusion of "+"s
@@ -38,13 +29,6 @@ for (let i = 3; i < nodeArgs.length; i++) {
     userTitle = (userTitle + " " + nodeArgs[i]).trim();
 }
 //console.log("song or movie requested:", userTitle);
-
-// Make it so liri.js can take in one of 
-//the following commands:
-// my-tweets
-// spotify-this-song
-// movie-this
-// do-what-it-says
 
 var requestType = process.argv[2];
 
@@ -63,10 +47,9 @@ switch (requestType) {
         break;
     case 'do-what-it-says':
         do_what_it_says();
-        console.log('do what it says');
         break;
     default:
-        console.log('Bad Request inputted');
+        console.log('Bad Request inputed');
 }
 
 //JohnPalumbo54
@@ -77,45 +60,45 @@ function get_my_tweets() {
         count: 20
     };
     client.get('search/tweets/', params, function (error, tweets, response) {
+        var msg = "";
         if (!error) {
             var tstat = tweets.statuses;  //tstat -- tweet status
             tstat.forEach(element => {
                 console.log(element.created_at, element.text);
+                msg += '\n' + element.created_at + ' - ' + element.text;
             });
         } else {
             console.log(" error in tweet ", error);
         }
-
+        writeLog("my Tweets", msg);
     });
 }
 
+
+//add tweets .
 function post_tweet() {
-    client.post('statuses/update',
-        { status: "#21  - surpassed" },
-        function (error, tweet, response) {
-            if (error) throw error;
-            console.log(tweet);  // Tweet body. 
-            //console.log(response);  // Raw response object. 
-        });
+    client.post('statuses/update', { status: "#23  - PayPal Has Been Quietly Getting Into the Traditional Banking Business" }, function (error, tweet, response) {
+        if (error) throw error;
+        console.log(tweet);  // Tweet body. 
+    });
 }
 
 
 function spotify_song(songname) {
-    spotify.search({ type: 'track', query: songname, limit: 50 }, function (err, data) {
+
+    if (songname == "") {
+        songname = 'ace of base the sign';
+    }
+
+    spotify.search({ type: 'track', query: songname, limit: 10 }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
-        }
-        // console.log('data.tracks.total', data.tracks.total);
-        // console.log("\n tracks object:", JSON.stringify(data.tracks));
-        if (data.tracks.total == 0) {
-            spotify_song("The Sign");
         }
 
         for (const key in data) {
             if (data.hasOwnProperty(key)) {
                 const element = data[key];
-                // console.log(++i, 'len: ' , element.items.length ,element);
-                // console.log('element.items: ',element.items);
+                var msg = "";
                 for (let i = 0; i < element.items.length; i++) {
                     const ele = element.items[i];
                     var artist = ele.album.artists[0].name;
@@ -123,7 +106,10 @@ function spotify_song(songname) {
                     var song = ele.external_urls.spotify;
                     var preview = ele.preview_url;
                     var alb = ele.album.name;
-                    console.log('\n\nArtist: ', artist, " , Album:   ", alb, "  Song Name: ", sngnm, ", \n Song URL:   ", song, " , Preview Url:  ", preview);
+                    msg = 'Artist: ' + artist + " , Album:   " + alb + "  Song Name: "
+                        + sngnm + "\nSong URL:   " + song + " , \nPreview Url:  " + preview;
+                    console.log(msg);
+                    writeLog('SPOTIFY', msg);
                 }
             }
         }
@@ -133,8 +119,6 @@ function spotify_song(songname) {
 }
 
 function movie_this(userTitle) {
-    //        * Title of the movie.  * Year the movie came out.  * IMDB Rating of the movie. * Rotten Tomatoes Rating of the movie.
-    //        * Country where the movie was produced. * Language of the movie. * Plot of the movie.  * Actors in the movie.
 
     var qsearch;  // for querystring- will hold title search and movie or id and movie    
 
@@ -144,10 +128,7 @@ function movie_this(userTitle) {
         qsearch = 't=' + userTitle;
     }
 
-
     var queryUrl = "http://www.omdbapi.com/?" + qsearch + "&apikey=trilogy&y=&plot=short&tomatoes=true&r=json";
-    //console.log(" the query:  ", queryUrl);
-    //mr nobody http://www.omdbapi.com/?i=tt0485947
 
     request(queryUrl, function (error, response, body) {
 
@@ -168,19 +149,24 @@ function movie_this(userTitle) {
     function writeMovie(oBody) {
         console.log(" This is function write Movie  - here is JSON of BODY", oBody);//bodyJson);
         var msg = '* Title of the movie: ' + oBody.Title + "\n* Year the movie came out: " + oBody.Year;
-        msg += '\n* IMDB Rating of the movie: ' + oBody.imdbRating + '\n* Rotten Tomatoes Rating of the movie: ' + oBody.tomatoRating;
+        msg += '\n* IMDB Rating of the movie: ' + oBody.imdbRating;
+        msg += '\n* Rotten Tomatoes Rating of the movie: ' + oBody.Ratings[1].Value + "," + oBody.tomatoRating;
         msg += '\n* Country where the movie was produced: ' + oBody.Country + "\n* Language of the movie:" + oBody.Language;
         msg += '\n* Plot of the movie: ' + oBody.Plot + '\n* Actors in the movie: ' + oBody.Actors;
-        console.log(msg);
+        writeLog("MOVIES", msg);
     }
 }
 
-function do_what_it_says(){
-    fs.readFile("random.txt", "utf8", function(err,data){
-        if (err){
+function do_what_it_says() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
             console.log("error in read file");
-            return(console.log(err));
+            return (console.log(err));
         }
+
+        //console.log("read: ", data);
+
+
         //read random.txt - create dataArray (use bank.js)
         dataArray = data.split(",");
         switch (dataArray[0]) {
@@ -201,8 +187,30 @@ function do_what_it_says(){
             //     console.log('do what it says');
             //     break;
             default:
-                console.log(' in do waht it says-- Bad Request inputted');
+                console.log(' in do what it says-- Bad Request inputted');
         }
 
     })
+}
+
+
+function writeLog(requestType, value) {
+    var date = new Date();
+    var header = '\n********** header for: ' + requestType + ' ** ' + date + '*************\n'
+    fs.appendFileSync('log.txt', header, 'utf8', function (err) {
+        if (err) {
+            console.log('Write Error ', requestType, err);
+        }
+    });
+    fs.appendFileSync('log.txt', value, 'utf8', function (err) {
+        if (err) {
+            console.log('Write Error ', requestType, err);
+        }
+    });
+    fs.appendFileSync('log.txt', '\n************************ footer for:' + requestType + "  ***********\n", 'utf8', function (err) {
+        if (err) {
+            console.log('Write Error ', requestType, err);
+        }
+    });
+
 }
